548e19c852ff11cf0b6f38b91c94fda84d0be0fb
cristipuraniu <85282178+cristipuraniu@users.noreply.github.com>
2022-06-06 17:08:52 +0300

    Add files via upload

---
+++ FahrenheitApp/FahrenheitApp/Models/Temperature.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FahrenheitApp.Models
+{
+    public static class Temperature
+    {
+        public static double CelciusToFahrenheit(double Tc)
+        {
+            double Tf;
+
+            Tf = 9 / 5 * Tc + 32;
+
+            return Tf;
+        }
+
+    }
+}
